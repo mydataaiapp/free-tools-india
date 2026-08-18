61b75b1cc21de80f024877b4432685b0006b2352
mydataaiapp <bestmarket0029@gmail.com>
2026-08-18 17:53:49 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,5 @@
-// Supabase Configuration
+// Supabase Configuration - Secure
 const SUPABASE_CONFIG = {
     SUPABASE_URL: 'https://ibosfyljujpwsyyjdurl.supabase.co',
-    SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_XF-3E1hw8_84grRf2Kl3zQ_J4kc0ENc',
-    SUPABASE_TABLE_FEEDBACK: 'feedback',
-    SUPABASE_TABLE_USERS: 'users'
+    SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_XF-3E1hw8_84grRf2Kl3zQ_J4kc0ENc'
 };
