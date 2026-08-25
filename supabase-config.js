6c16eb29c87137017ef33451f8862c34c32f1302
mydataaiapp <bestmarket0029@gmail.com>
2026-08-25 06:09:10 +0530

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-// Supabase Configuration - Secure
-const SUPABASE_CONFIG = {
-    SUPABASE_URL: 'https://ibosfyljujpwsyyjdurl.supabase.co',
-    SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_XF-3E1hw8_84grRf2Kl3zQ_J4kc0ENc'
-};
